0eec06cb9c994d0472b49edee0a68c31975a8e56
Abdullah Al Nahdi <al.nahdi.tnmy@gmail.com>
2020-10-24 17:59:45 +0600

    Event handling challenge completed using JS events

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const bgColorCode = '#8e44ad'
+  const [bgColor, setBgColor] = useState(bgColorCode)
+
+  const buttonTitle = 'Click Here'
+  const [btnTitle, setBtnTitle] = useState(buttonTitle)
+
+  const bgChange = () => {
+    // console.log('clicked')
+    let newBgColorCode = '#34495e';
+    setBgColor(newBgColorCode);
+
+    let newBtnTitle = 'Oppss!! 😮 ';
+    setBtnTitle(newBtnTitle);
+  }
+
+  const doubleClick = () => {
+    let newBgColorCode = '#033669';
+    setBgColor(newBgColorCode);
+    setBtnTitle('Congrats!! ㊗️ ');
+  };
+
   return (
-    <div className="App">
-      <h1>Helllo!!!</h1>
+    <div className="App" style={{ backgroundColor: bgColor }}>
+      <button onMouseEnter={bgChange} onMouseLeave={doubleClick}>{btnTitle}</button>
     </div>
   );
 }
